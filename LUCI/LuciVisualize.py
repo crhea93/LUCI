b4b2d2a868f27fbe41671eb1155caf3033ab6458
Carter Rhea <carterrhea93@gmail.com>
2022-04-08 14:39:41 -0400

    Added rectangles

--- LUCI/LuciVisualize.py
+++ LUCI/LuciVisualize.py
@@ -4,6 +4,8 @@ Luci visualization tools
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.patches import Rectangle
+from matplotlib.widgets  import RectangleSelector
 
 def visualize(deep_image, spectrum_axis, cube_final):
     """
@@ -12,43 +14,59 @@ def visualize(deep_image, spectrum_axis, cube_final):
     """
     fig,axes = plt.subplots(2,1,figsize=(15,15))
     plt.style.use('fivethirtyeight')
-    shift_is_held = False
+
     shift_ct = 0
-
-    def on_key_press(event):
-        print(event.key)
-        if event.key == 'shift':
-            shift_is_held = True
-            return True
-        else:
-            return False
-
-
-    def on_key_release(event):
-        if event.key == 'shift':
-            shift_is_held = False
-        return False
-
-    shift_is_held = fig.canvas.mpl_connect('key_press_event', on_key_press)
-    shift_is_held = fig.canvas.mpl_connect('key_release_event', on_key_release)
-
     point1 = []
+
+    rectangles = []
+
+
+    def line_select_callback(eclick, erelease):
+        x1, y1 = eclick.xdata, eclick.ydata
+        x2, y2 = erelease.xdata, erelease.ydata
+
+        rect = plt.Rectangle((min(x1, x2), min(y1, y2)), np.abs(x1 - x2), np.abs(y1 - y2))
+        if len(rectangles) > 0:
+            rectangles[-1].remove()
+        rectangles.append(rect)
+
+        axes[0].add_patch(rect)
+        integrated_spectrum = np.zeros(cube_final.shape[2])
+        for i in range(int(y2 - y1)):
+            y_pix = int(y1 + i)
+            for j in range(int(x2 - x1)):
+                x_pix = int(x1 + j)
+                # Check if pixel is in the mask or not
+                integrated_spectrum += cube_final[x_pix, y_pix, :]
+        axes[1].cla()
+        axes[1].set_title('Spectrum of region')
+        plt.plot(1e7 / spectrum_axis, integrated_spectrum, linewidth=1)
+        axes[1].set_xlabel('Wavelength [nm]', fontweight='bold')
+        axes[1].set_ylabel(r'Intensity (Ergs/cm$^2$/s/$\AA$)', fontweight='bold')
+        print('spectrum of region')
+        print(rectangles)
 
     #deep_image = fits.open('Luci_outputs/NGC628_deep.fits')[0].data
     def onclick(event):
         plt.subplot(212)
-
-        print(shift_is_held)
+        global shift_ct
+        global point1
+        if event.key == 'control':
+            shift_is_held = True
+        else:
+            shift_is_held = False
+            point1 = []
+            shift_ct = 0
         if shift_is_held is True:
             print('SHIFT %i %i'%(event.xdata, event.ydata))
-            axes[0].plot(int(event.xdata), int(event.ydata), 'o', color='y')
-            if shift_ct == 0:
+            #axes[0].plot(int(event.xdata), int(event.ydata), 'o', color='y')
+            if shift_ct != 1:
                 point1 = [int(event.xdata), int(event.ydata)]
-                shift_ct += 1
-            if shift_ct == 1:
-                dist = np.sqrt((int(event.xdata)-point1[0])**2+(int(event.ydata)-point[1])**2)
+                shift_ct = 1
+            if len(point1) == 1:
+                dist = np.sqrt((int(event.xdata)-point1[0])**2+(int(event.ydata)-point1[1])**2)
                 circle = plt.Circle((point1[0], point1[1]), dist, color='b', fill=False)
-                shift_ct = 0
+                point1 = []
         else:
             X_coordinate = int(event.xdata)
             Y_coordinate = int(event.ydata)
@@ -58,12 +76,16 @@ def visualize(deep_image, spectrum_axis, cube_final):
             axes[1].set_xlabel('Wavelength [nm]', fontweight='bold')
             axes[1].set_ylabel(r'Intensity (Ergs/cm$^2$/s/$\AA$)', fontweight='bold')
             plt.show()
-            shift_ct = 0
 
+    fig.canvas.mpl_connect('button_press_event', onclick)
 
     plt.subplot(211)
-    fig.canvas.mpl_connect('button_press_event', onclick)
-    plt.imshow(np.log10(deep_image))
-    plt.get_current_fig_manager().toolbar.zoom()
 
+    plt.imshow(np.log10(deep_image))
+
+    rs = RectangleSelector(axes[0], line_select_callback,
+                           drawtype='box', useblit=False, button=[1],
+                           minspanx=5, minspany=5, spancoords='pixels',
+                           interactive=False)
+    print(rectangles)
     plt.show()
