ac34216e28da6bd12f96d79747b82517c38e1f8d
Carter Rhea <carterrhea93@gmail.com>
2022-05-19 11:12:01 +0200

    Turned off fit results in real time

--- LUCI/LuciFit.py
+++ LUCI/LuciFit.py
@@ -526,7 +526,7 @@ class Fit:
         # We do **not** use the interpolated spectrum here!
         soln = minimize(nll, initial,
                         method='SLSQP',
-                        options={'disp': True, 'maxiter': 30},
+                        options={'disp': False, 'maxiter': 30},
                         tol=1e-2,
                         args=(), constraints=cons
                         )
