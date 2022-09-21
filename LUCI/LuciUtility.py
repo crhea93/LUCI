Removed extra [ ] in LuciBase.py Changed bin_cube_function() in LUCI/LuciUtility.py, to fix CRPIX formula

LUCI/LuciUtility.py
@@ -201,7 +201,7 @@ def update_header(file):
         try:
             if header_col == 'flambda':
                 hdr_dict['flambda'] = header_val
-                clean_hdr_dict['flambda'] = header_val
+                #clean_hdr_dict['flambda'] = header_val
             if header_type is np.float64:
                 hdr_dict[header_col] = float(header_val)
                 clean_hdr_dict[header_col] = float(header_val)
@@ -327,10 +327,10 @@ def bin_cube_function(cube_final, header, binning, x_min, x_max, y_min, y_max):
             summed_spec = np.nansum(summed_spec, axis=0)
             binned_cube[i, j] = summed_spec[:]
     header_binned = header
-    header_binned['CRPIX1'] = header_binned['CRPIX1'] / binning
-    header_binned['CRPIX2'] = header_binned['CRPIX2'] / binning
-    header_binned['CDELT1'] = header_binned['CDELT1'] * binning
-    header_binned['CDELT2'] = header_binned['CDELT2'] * binning
+    header_binned['CRPIX1'] = (header_binned['CRPIX1']-x_min-0.5) / binning + 0.5
+    header_binned['CRPIX2'] = (header_binned['CRPIX2']-y_min-0.5) / binning + 0.5
+    #header_binned['CDELT1'] = header_binned['CDELT1'] * binning
+    #header_binned['CDELT2'] = header_binned['CDELT2'] * binning
     header_binned['PC1_1'] = header_binned['PC1_1'] * binning
     header_binned['PC1_2'] = header_binned['PC1_2'] * binning
     header_binned['PC2_1'] = header_binned['PC2_1'] * binning

LuciBase.py
@@ -202,8 +202,12 @@ class Luci():
             header_binned = self.header
             header_binned['CRPIX1'] = header_binned['CRPIX1'] / binning
             header_binned['CRPIX2'] = header_binned['CRPIX2'] / binning
-            header_binned['CDELT1'] = header_binned['CDELT1'] * binning
-            header_binned['CDELT2'] = header_binned['CDELT2'] * binning
+            #header_binned['CDELT1'] = header_binned['CDELT1'] * binning
+            #header_binned['CDELT2'] = header_binned['CDELT2'] * binning
+            header_binned['PC1_1'] = header_binned['PC1_1'] * binning
+            header_binned['PC1_2'] = header_binned['PC1_2'] * binning
+            header_binned['PC2_1'] = header_binned['PC2_1'] * binning
+            header_binned['PC2_2'] = header_binned['PC2_2'] * binning
             self.deep_image = binned_deep / (binning ** 2)
         if output_name == None:
             output_name = self.output_dir + '/' + self.object_name + '_deep.fits'
@@ -308,7 +312,7 @@ class Luci():
                     sky -= bkg * binning ** 2  # Subtract background spectrum
                 else:  # No binning so just subtract the background directly
                     sky -= bkg  # Subtract background spectrum
-            good_sky_inds = [~np.isnan(sky)]  # Find all NaNs in sky spectrum
+            good_sky_inds = ~np.isnan(sky)  # Find all NaNs in sky spectrum
             sky = sky[good_sky_inds]  # Clean up spectrum by dropping any Nan values
             axis = self.spectrum_axis[good_sky_inds]  # Clean up axis  accordingly
             if initial_values[0] is not False:   #Frozen parameter