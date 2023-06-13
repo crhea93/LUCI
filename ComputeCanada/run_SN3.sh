b7953bd97369da06abd8a5c62bd5c30f9816ece7
Carter Lee Rhea <carterrhea93@gmail.com>
2023-06-13 09:48:26 -0400

    Update run_SN3.sh
    Update compute canada bash call after discussions with Compute Canada Scientists.

--- ComputeCanada/run_SN3.sh
+++ ComputeCanada/run_SN3.sh
@@ -3,11 +3,9 @@
 #SBATCH --time=40:00:00                                                                    
 #SBATCH --job-name=NGC4449_SN3                                                      
 #SBATCH --output=%x.out                                                                    
-#SBATCH --mem-per-cpu=16Gb                                                               
-#SBATCH --cpus-per-task=1               # maximum cpu per task is 3.5 per gpus 
-#SBATCH --tasks=100
-#SBATCH --ntasks-per-node=25
-module load cuda cudnn 
+#SBATCH --mem-per-cpu=4Gb                                                               
+#SBATCH --cpus-per-task=40              
+#SBATCH --nodes=1
 module load scipy-stack
 source luci/bin/activate
 python run_SN3.py
