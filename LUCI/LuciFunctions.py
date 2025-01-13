fa36e6c754c79d446fdfcc98c54714f00b54a31d
Carter Rhea <carterrhea93@gmail.com>
2025-01-13 11:08:39 -0500

    Divide the sincwidth by the FWHM of sinc when fitting sincgauss function.

--- LUCI/LuciFunctions.py
+++ LUCI/LuciFunctions.py
@@ -222,7 +222,7 @@ class SincGauss:
     def function(self, channel, params, sinc_width):
         p0 = params[0]
         p1 = params[1]
-        p2 = sinc_width/np.pi
+        p2 = sinc_width/(np.pi*FWHM_SINC_COEFF)
         p3 = params[2]
         a = p3/(np.sqrt(2)*p2)
         a = a.astype(float)
