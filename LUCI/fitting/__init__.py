2ddc6c2c7628205799136bfa8f38306517f69e61
carter rhea <carterrhea93@gmail.com>
2026-07-23 15:50:12 -0400

    refactor fitting module

--- LUCI/fitting/__init__.py
+++ LUCI/fitting/__init__.py
@@ -1,5 +1,26 @@
 """Fitting: line models, parameter conversions, and the fit result type."""
 
+from LUCI.fitting.models import Gaussian, Sinc, SincGauss, frozen_values
+from LUCI.fitting.parameters import (
+    calculate_broad,
+    calculate_broad_err,
+    calculate_flux,
+    calculate_flux_err,
+    calculate_vel,
+    calculate_vel_err,
+)
 from LUCI.fitting.result import FitResult
 
-__all__ = ["FitResult"]
+__all__ = [
+    "FitResult",
+    "Gaussian",
+    "Sinc",
+    "SincGauss",
+    "frozen_values",
+    "calculate_vel",
+    "calculate_vel_err",
+    "calculate_broad",
+    "calculate_broad_err",
+    "calculate_flux",
+    "calculate_flux_err",
+]
